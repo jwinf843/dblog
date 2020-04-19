8baee6a52607cc6bf5699d5e0f75d6edaedef193
Jordan Winfery <jwinf843@gmail.com>
2020-04-19 18:36:20 +0900

    Forgot a comma that caused the webapp to crash...

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-	return HttpResponse('<h1>Blog Home</h1>')
+	return render(request, 'blog_templates/home.html')
 
 def about(request):
-	return HttpResponse('<h1>Blog About</h1>')
+	return render(request, 'blog_templates/about/html')
