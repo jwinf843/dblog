d81e05ffe14d81fe645f80cf049f90866f1908f4
Jordan Winfery <jwinf843@gmail.com>
2020-04-21 13:43:35 +0900

    Fixed error where views.py wasnt properly rendering the about page

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
+# def home(request):
+# 	return HttpResponse('<h1>Blog Home</h1>') # The old way
+
 def home(request):
 	return render(request, 'blog_templates/home.html')
 
 def about(request):
-	return render(request, 'blog_templates/about/html')
+	return render(request, 'blog_templates/about.html')
