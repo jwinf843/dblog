41269f3d82597a0357197463121d3d72ebb08381
Jordan Winfery <jwinf843@gmail.com>
2020-04-21 15:12:33 +0900

    Added dummy data to the views.py file to practice html injection.

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse # Not using it, don't need it
 # Create your views here.
 
 # def home(request):
 # 	return HttpResponse('<h1>Blog Home</h1>') # The old way
 
+posts = [
+	{
+		'author': 'Jordan',
+		'title': 'Post 1', 
+		'content': 'Post Content 1',
+		'date': '20.04.2020',
+	},
+	{
+		'author': 'Jordan',
+		'title': 'Post 2', 
+		'content': 'Post Content 2',
+		'date': '21.04.2020',
+	}
+]
+
 def home(request):
-	return render(request, 'blog_templates/home.html')
+	context = {
+		'posts': posts
+	}
+	return render(request, 'blog_templates/home.html', context)
 
 def about(request):
 	return render(request, 'blog_templates/about.html')
